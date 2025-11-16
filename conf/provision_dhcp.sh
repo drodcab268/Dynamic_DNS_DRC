fbe62caa90ecc4b54ad8fd095684882809cf9325
drodcab268 <danielroca26@gmail.com>
2025-11-16 19:34:59 +0100

    provision dhcp update, error correction and comments

--- conf/provision_dhcp.sh
+++ conf/provision_dhcp.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+set -xeu
+
 # Installing ISC-DHCP-Server
-apt update
-apt install isc-dhcp-server sshpass -y
+apt-get update
+apt-get install -y isc-dhcp-server net-tools iproute2
+
+# Deploying DHCP configuration
+cp /vagrant/conf/dhcpd.conf /etc/dhcp/dhcpd.conf
 
 # Configure DHCP to listen on eth1
-echo 'INTERFACESv4="eth1"' > /etc/default/isc-dhcp-server
-echo 'INTERFACESv6=""' >> /etc/default/isc-dhcp-server
+sed -i 's/^INTERFACESv4=.*/INTERFACESv4="eth1"/' /etc/default/isc-dhcp-server
 
 # Copying TSIG key from shared folder
 cp /vagrant/ddns.key /etc/dhcp/ddns.key
 
-# Deploying DHCP configuration
-cp /vagrant/dhcpd.conf /etc/dhcp/dhcpd.conf
-
 # Restart ISC DHCP server
 systemctl restart isc-dhcp-server
+systemctl enable isc-dhcp-server
+systemctl status isc-dhcp-server --no-pager
